f565c24cc1bca3c980c2efe22ae07902c2001058
Luiz Budeu <luguibudeu1@gmail.com>
2024-10-29 16:50:52 -0300

    start of shifting bar. replaced free saved gates in toolbox, now inside shifting bar. needs rotation

--- public/engine/gui/toolbox.js
+++ public/engine/gui/toolbox.js
@@ -3,6 +3,7 @@ import Bridge from "../bridge.js";
 import Settings from "../settings.js";
 import SavedGate from "./savedGate.js";
 import SaveManager from "../managers/saveManager.js";
+import ShiftingBar from "../UIComponents/shiftingBar.js";
 
 class Toolbox {
     constructor(canvas, ctx) {
@@ -16,10 +17,12 @@ class Toolbox {
 
     start() {
         const height = Settings.TOOLBOX_HEIGHT;
-        this.rect = new Rect(this.ctx)
+        this.backgroundRect = new Rect(this.ctx)
             .at(0, this.canvas.height - height)
             .size(this.canvas.width, height)
             .color("#0C0C0C");
+
+        this.shiftingBar = new ShiftingBar(this.ctx).at(0, this.canvas.height - height).size(this.canvas.width, height);
 
         this.loadSavedGates();
     }
@@ -29,28 +32,20 @@ class Toolbox {
     }
 
     setupSavedGates(savedGatesData) {
-        const toolboxPos = this.rect.at();
-        let lastSavedGateX = 0;
-
         savedGatesData.forEach((gateObj, index) => {
             const savedGate = new SavedGate(this.ctx, this, gateObj.name, gateObj.logicFunction, gateObj.ios);
             savedGate.start();
-
-            const gateX = toolboxPos.x + lastSavedGateX + Settings.SAVED_GATE_BASE_MARGIN.x;
-            const gateY = toolboxPos.y + Settings.SAVED_GATE_BASE_MARGIN.y;
-
-            lastSavedGateX += savedGate.rect.width + Settings.SAVED_GATE_BASE_MARGIN.x;
-
-            savedGate.rect.at(gateX, gateY);
-            savedGate.rect.innerText.centerInRect(savedGate.rect);
-            Bridge.sceneInstance.place(savedGate, 0);
+            this.shiftingBar.addOption(savedGate);
         });
+
+        Bridge.sceneInstance.place(this.shiftingBar, Settings.UI_LAYER, true);
     }
 
     update() {}
 
     draw() {
-        this.rect.draw();
+        this.backgroundRect.draw();
+        this.shiftingBar.draw();
     }
 }
 
