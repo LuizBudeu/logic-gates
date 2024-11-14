9242579fd5b93d24061347cebd273d5d96b44900
DamaralHenrique <rique01101999@gmail.com>
2024-11-13 21:00:40 -0300

    Ajusta termplate base da documentacao

--- front/src/interfaces/Documentation.js
+++ front/src/interfaces/Documentation.js
@@ -1,13 +1,16 @@
 import logo from './../logo.svg';
 import React from "react";
 import { Header } from "../components/Header.js"
+import { Background } from '../components/Background.js';
 
 export const Documentation = () => {
 
 	return(
 		<div className="App">
 			<Header/>
-			<h3>Documentation</h3>
+			<Background>
+				<text>Documentation</text>
+			</Background>
 		</div>
 	)
 
