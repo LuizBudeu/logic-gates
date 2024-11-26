e72a5c5d2a259d20ab9d449ac56d2991bc671c52
Luiz Budeu <luguibudeu1@gmail.com>
2024-11-26 15:50:30 -0300

    start of activity seed

---
+++ server/api/activity_seed.py
@@ -0,0 +1,31 @@
+from .models import Activity
+
+
+
+
+activities = [
+    'NOT',
+    'AND',
+    'OR',
+    'NOR',
+    'XOR',
+    'HALFADDER',
+    'FULLADDER',
+    'FOURBITADDER',
+    'MUX4X1',
+    'DMUX1X4',
+    'ENCODER4X2',
+    'DECODER2X4',
+    'ALU',
+    'SRLATCH',
+    'DLATCH',
+    'DFLIPFLOP',
+    'REGISTER',
+    'COUNTER',
+    'ROM',
+    'RAM'
+]
+
+
+
+
